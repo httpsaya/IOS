887733c831a6ceb2e5f0fe7dd18777090988843c
Saya Mirkulova <zansaaumirkulova@gmail.com>
2026-09-18 20:46:09 +0500

    second

--- first/ContentView.swift
+++ first/ContentView.swift
@@ -31,5 +31,28 @@ struct ContentView: View {
 
 func someCoding(){
     
+//    Create an array of five diﬀerent fruits. Access and print the third fruit in the array.
+    var array1: [String] = ["Apple", "Strawberry", "Cherry", "Melon", "Watermelon"]
+    print(array1[4])
+    
+//    Create a set of your favorite numbers. Add a new number to the set and print the updated set.
+    var set: Set<Int> = [26, 7, 11]
+    
+    set.insert(4)
+    print(set)
+    
+/*
+     Create a dictonary with three key-value pairs where the keys are names of programming
+     languages and the values are their release years. Access and print the release year of
+     Swift.
+*/
+    var dict: [String: Int] = ["C": 1972, "Java": 1995, "Python": 1991]
+    print(dict)
+    
+//    Create an array of four colors. Update the second color to a new one and print the updated array.
+    var array2: [String] = ["red", "yellow", "pink", "black"]
+    array2[1] = "blue"
+    
+    print(array2)
     
 }
